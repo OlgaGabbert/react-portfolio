43c81e6cb7430ac759c70f963518da826b380673
OlgaGabbert <olga.gabbert@gmail.com>
2023-11-30 19:43:12 +0100

    add new js file for projects

--- src/App.js
+++ src/App.js
@@ -3,34 +3,10 @@ import React from 'react';
 import Navbar from './components/navbar/Navbar';
 import Projects from './components/projects/Projects';
 import Footer from './components/footer/Footer';
+import projectsData from './projectsData';
 
 function App() {
-  const projectData = [
-    {
-      imgSrc: 'JokesProject.png',
-      alt: 'Jokes project',
-      title: 'Jokes from APIs',
-      description: 'Description of Project 1.',
-      technologiesUsed: ["Node.js", "Express", "MongoDB"],
-      link: '#',
-    },
-    {
-      imgSrc: 'To-doProject.png',
-      alt: 'To-do-app',
-      title: 'To-do-App',
-      description: 'Description of Project 2.',
-      technologiesUsed: ["React", "Firebase"],
-      link: '#',
-    },
-    {
-      imgSrc: 'AnotherProject.png',
-      alt: 'Project Name',
-      title: 'Project 3',
-      description: 'Description of Project 3.',
-      link: '#',
-      technologiesUsed: ["HTML", "CSS", "JavaScript"],
-    },
-  ];
+  
   return (
     <div>
       <Navbar className='my-navbar'
@@ -38,7 +14,7 @@ function App() {
         linkSrc="#"
         link="#"
         projectsLink="#" />
-      <Projects className='my-projects' projectData={projectData}/>
+      <Projects className='my-projects' projectData={projectsData}/>
       <Footer className='my-footer' contactLink="#" />
     </div>
   );
---
+++ src/projectsData.js
@@ -0,0 +1,28 @@
+const projectsData = [
+    {
+      imgSrc: 'JokesProject.png',
+      alt: 'Jokes project',
+      title: 'Jokes from APIs',
+      description: 'Description of Project 1.',
+      technologiesUsed: ["Node.js", "Express", "MongoDB"],
+      link: '#',
+    },
+    {
+      imgSrc: 'To-doProject.png',
+      alt: 'To-do-app',
+      title: 'To-do-App',
+      description: 'Description of Project 2.',
+      technologiesUsed: ["React", "Firebase"],
+      link: '#',
+    },
+    {
+      imgSrc: 'AnotherProject.png',
+      alt: 'Project Name',
+      title: 'Project 3',
+      description: 'Description of Project 3.',
+      link: '#',
+      technologiesUsed: ["HTML", "CSS", "JavaScript"],
+    },
+  ];
+
+  export default projectsData;
